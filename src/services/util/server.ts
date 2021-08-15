4920398335bed7b05006c120054ef40d52b1c9ca
Jack LaFond <jack.lafond@icloud.com>
2021-08-15 10:51:48 -0400

    fix(server): set server port to 80

--- src/services/util/server.ts
+++ src/services/util/server.ts
@@ -1,19 +1,21 @@
 import express from "express";
 import * as TopGG from "@top-gg/sdk";
-import {redis} from "../redis";
+import { redis } from "../redis";
 import * as logs from "../events/logging";
-import {AbstractAppService} from "./abstract-app-service";
+import { AbstractAppService } from "./abstract-app-service";
 
 const SIX_HOURS_IN_SECONDS = 60 * 60 * 6;
 
 export class VotesServer extends AbstractAppService {
   private readonly app = express();
-  private readonly webhook = new TopGG.Webhook(`${process.env.TOPGG_WEBHOOK_AUTH}`);
+  private readonly webhook = new TopGG.Webhook(
+    `${process.env.TOPGG_WEBHOOK_AUTH}`
+  );
 
   start(): void {
     this.app.post(
       "/vote",
-      this.webhook.listener(async vote => {
+      this.webhook.listener(async (vote) => {
         const user = this.client.users.cache.get(vote.user);
 
         await redis.set(
@@ -27,6 +29,6 @@ export class VotesServer extends AbstractAppService {
       })
     );
 
-    this.app.listen(process.env.SERVER_PORT || 8080);
+    this.app.listen(process.env.SERVER_PORT || 80);
   }
 }
