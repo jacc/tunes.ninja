5b58631f9b72f00c6d345b2fcde9258d0219e458
Leah Lundqvist <leah@pigeon.sh>
2021-08-22 20:28:10 +0200

    ci: use port 80 for deployment

--- src/services/util/server.ts
+++ src/services/util/server.ts
@@ -29,6 +29,6 @@ export class VotesServer extends AbstractAppService {
       })
     );
 
-    this.app.listen(process.env.SERVER_PORT || 8097);
+    this.app.listen(process.env.SERVER_PORT || 80);
   }
 }
