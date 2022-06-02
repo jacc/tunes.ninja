cc1c8eab1252711af0a99262fefb1066686704fa
Mustafa Mohamed <git@ms7m.me>
2022-06-01 20:28:15 -0400

    fix(errors): add errors for unauthorized additions to playlists

--- dojo/shared/error_codes/add_to_playlist_errors.py
+++ dojo/shared/error_codes/add_to_playlist_errors.py
@@ -10,7 +10,14 @@ class AppleMusicUnableToAddToPlaylists(ErrorMessage):
 class SpotifyUnableToAddToPlaylists(ErrorMessage):
     error_code = "spo_001"
     error_message = "Unable to add song to Spotify playlist."
-    
+
+class AppleMusicUnauthorizedUnableToAddToPlaylists(ErrorMessage):
+    error_code = "apl_002"
+    error_message = "This user is not authorized to add songs to Apple Music playlists"
+
+class SpotifyUnauthorizedUnableToAddToPlaylists(ErrorMessage):
+    error_code = "spo_002"
+    error_message = "This user is not authorized to add songs to Spotify playlists."
     
     
 error_codes = [
