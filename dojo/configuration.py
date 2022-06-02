69c59111b68c1486e956850aa6181a7a0b7eff98
Mustafa Mohamed <git@ms7m.me>
2022-06-02 19:32:01 -0400

    chore(settings): add celery settings

--- dojo/configuration.py
+++ dojo/configuration.py
@@ -9,7 +9,8 @@ class Configuration(BaseSettings):
     DOJO_MONGODB_URL: str
     DOJO_MONGODB_DATABASE_NAME: str
     DOJO_REDIS_URL: str
-
+    DOJO_CELERY_REDIS_BROKER_URL: str
+    DOJO_CELERY_REDIS_RESULTS_URL: str
     
     class Config:
         env_file = ".env.prod"
