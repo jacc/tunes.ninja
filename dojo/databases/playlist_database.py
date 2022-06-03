f622394e9e3520b5268d135c8e5e5622999db095
Mustafa Mohamed <git@ms7m.me>
2022-06-03 02:36:07 -0400

    fix: temp use str type for last_updated (new type coming soon)

--- dojo/databases/playlist_database.py
+++ dojo/databases/playlist_database.py
@@ -1,14 +1,18 @@
 
 from datetime import datetime
 from odmantic import Model
+from pydantic import HttpUrl
 from dojo.shared.supported_services import SupportedServices
 import typing
+from dojo.shared.type_hint_models import enable_init_auto_complete
 
+
+@enable_init_auto_complete
 class UserCreatedPlaylists(Model):
     
     service: SupportedServices
     service_unique_id: str
     linked_to_discord_user: str
-    last_updated: datetime
-    
-    automatic_sync_channel_id: typing.Optional[str] = None
+    last_updated: str
+    linked_to_discord_channel: typing.Optional[str] = None
+    service_playlist_url: typing.Optional[HttpUrl] = None
