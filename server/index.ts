00957b896b70857c03eaeb283069b3db7f1db434
Jack LaFond <hi@jack.link>
2023-01-29 22:23:39 -0500

    small things etc

---
+++ server/index.ts
@@ -0,0 +1,33 @@
+import { initTRPC, inferAsyncReturnType } from "@trpc/server";
+import { z } from "zod";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import express from "express";
+
+// created for each request
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({}); // no context
+type Context = inferAsyncReturnType<typeof createContext>;
+const t = initTRPC.context<Context>().create();
+
+export const appRouter = t.router({
+  getUser: t.procedure.input(z.string()).query((req) => {
+    req.input; // string
+    return { id: req.input, name: "Bilbo" };
+  }),
+});
+
+const app = express();
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
+
+export type AppRouter = typeof appRouter;
+
+app.listen(4000);
